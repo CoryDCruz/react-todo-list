556ac37a71911d99ba96f56ace4e25b7745a67ce
Cory Dela Cruz <cory.delacruz@housecallpro.com>
2022-10-14 12:04:42 -0700

    initial setup with functions and form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import TodoForm from './TodoForm'
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
 
 function App() {
+  const [todos, setTodos] = React.useState(
+    [
+      {
+        todo: "Foo",
+        done: false,
+      }
+    ])
+
+    const addTodo = text => {
+      const todoList = [...todos, { text }]
+      setTodos(todoList)
+    }
+
+    const completeTodo = index => {
+      const todoList = [...todos]
+      todoList[index].done = true
+      setTodos(todoList)
+    }
+
+    const deleteTodo = index => {
+      const todoList = [...todos]
+      todoList.splice(index, 1)
+      setTodos(todoList)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <TodoForm addTodo={addTodo}/>
+      <div>
+        {todos.map((todo, index) => {
+          return(
+            <Card>
+            <Card.Body>
+
+            </Card.Body>
+          </Card>
+          )
+          
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/TodoForm.js
@@ -0,0 +1,8 @@
+function TodoForm({ addTodo }){
+  
+  return (
+    <h1>Todo Form</h1>
+  )
+}
+
+export default TodoForm
